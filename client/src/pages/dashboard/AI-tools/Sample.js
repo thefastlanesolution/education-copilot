48a5b66b71a007c36936ca4ad89dd3e719cfb92c
Devin <you@example.com>
2023-01-01 17:28:21 -0800

    Mobile Test #2

---
+++ client/src/pages/dashboard/AI-tools/Sample.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+import './Sample.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
